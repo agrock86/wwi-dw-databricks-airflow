9d8c5665f0498beaf89974d941f0065f628591f7
Ricardo Patiño <twinsensor@hotmail.com>
2025-02-12 23:48:39 -0500

    Add bicep templates to deploy infra as code

--- infra/setup_airflow.sh
+++ infra/setup_airflow.sh
@@ -4,6 +4,6 @@ sudo sh setup_docker.sh
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.10.4/docker-compose.yaml'
 
 mkdir -p ./dags ./logs ./plugins ./config
-echo -e "AIRFLOW_UID=$(id -u)" > .env
+echo -e "AIRFLOW_UID=$(id -u)" > .env.docker
 
-docker compose up airflow-init
+docker compose --env-file .env.docker config up airflow-init
