1b63dd638b65ecd25a2b353a14ce4cbd8b1409e2
Ricardo Patiño <twinsensor@hotmail.com>
2025-02-13 19:16:25 -0500

    Add bicep templates to deploy infra as code

--- infra/setup_airflow.sh
+++ infra/setup_airflow.sh
@@ -1,9 +1,24 @@
-while getopts a: flag
-do
-    case "${flag}" in
-        a) airflow_dir=${OPTARG};;
+airflow_dir=""
+
+# parse arguments manually
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -airflow_dir)
+            airflow_dir="$2"
+            shift 2 # shift past argument and its value
+            ;;
+        *)
+            echo "Usage: $0 -airflow_dir <path>"
+            exit 1
+            ;;
     esac
 done
+
+# check if airflow_dir is set
+if [ -z "$airflow_dir" ]; then
+    echo "Error: Missing required argument -airflow_dir <path>"
+    exit 1
+fi
 
 mkdir -p $airflow_dir
 cd $airflow_dir
--- infra/setup_git.sh
+++ infra/setup_git.sh
@@ -1,27 +1,40 @@
-while getopts airflow_dir: flag
-do
-    case "${flag}" in
-        airflow_dir) airflow_dir=${OPTARG};;
+# parse arguments manually
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -airflow_dir)
+            airflow_dir="$2"
+            shift 2 # shift past argument and its value
+            ;;
+        *)
+            echo "Usage: $0 -airflow_dir <path>"
+            exit 1
+            ;;
     esac
 done
 
-# Define variables
+# check if airflow_dir is set
+if [ -z "$airflow_dir" ]; then
+    echo "Error: Missing required argument -airflow_dir <path>"
+    exit 1
+fi
+
+# define variables
 REPO_URL="https://github.com/agrock86/wwi-dw-databricks-airflow.git"
 BRANCH="main"
 DAGS_DIR="$airflow_dir/dags"
 TEMP_DIR="$airflow_dir/temp"
 
-# Check if the temp directory exists
+# check if the temp directory exists
 if [ ! -d "$TEMP_DIR" ]; then
-  # Clone the repository if not already cloned
+  # clone the repository if not already cloned
   git clone --branch "$BRANCH" "$REPO_URL" "$TEMP_DIR"
 else
-  # Pull the latest changes
+  # pull the latest changes
   cd "$TEMP_DIR" || exit
-  git reset --hard  # Ensure no local changes interfere
-  git clean -fd     # Remove untracked files
+  git reset --hard  # ensure no local changes interfere
+  git clean -fd     # remove untracked files
   git pull origin "$BRANCH"
 fi
 
-# Sync only Python files from the 'dag' folder in the repository to the Airflow DAGs folder
+# sync only Python files from the 'dag' folder in the repository to the Airflow DAGs folder
 rsync -av --include="*.py" --exclude="*" "$TEMP_DIR/dag/" "$DAGS_DIR/"
