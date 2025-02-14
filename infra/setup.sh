689dc098d3ea5027340a53eb5bdd28af759b2f8c
Ricardo Patiño <twinsensor@hotmail.com>
2025-02-13 19:32:38 -0500

    Add bicep templates to deploy infra as code

--- infra/setup.sh
+++ infra/setup.sh
@@ -1,24 +1,20 @@
+# default value
 airflow_dir=""
 
-# parse arguments manually
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        -airflow_dir)
-            airflow_dir="$2"
-            shift 2 # Shift past argument and its value
-            ;;
-        *)
-            echo "Usage: $0 -airflow_dir <path>"
-            exit 1
-            ;;
-    esac
+# parse argument
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --airflow_dir=*) airflow_dir="${1#*=}";;
+    *) echo "Unknown parameter: $1"; exit 1;;
+  esac
+  shift
 done
 
-# check if airflow_dir is set
+# validate input
 if [ -z "$airflow_dir" ]; then
-    echo "Error: Missing required argument -airflow_dir <path>"
-    exit 1
+  echo "Error: --airflow_dir parameter is required."
+  exit 1
 fi
 
 sudo ./setup_docker.sh
-sudo ./setup_airflow.sh -airflow_dir $airflow_dir
+sudo ./setup_airflow.sh --airflow_dir=$airflow_dir
--- infra/setup_airflow.sh
+++ infra/setup_airflow.sh
@@ -1,23 +1,19 @@
+# default value
 airflow_dir=""
 
-# parse arguments manually
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        -airflow_dir)
-            airflow_dir="$2"
-            shift 2 # shift past argument and its value
-            ;;
-        *)
-            echo "Usage: $0 -airflow_dir <path>"
-            exit 1
-            ;;
-    esac
+# parse argument
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --airflow_dir=*) airflow_dir="${1#*=}";;
+    *) echo "Unknown parameter: $1"; exit 1;;
+  esac
+  shift
 done
 
-# check if airflow_dir is set
+# validate input
 if [ -z "$airflow_dir" ]; then
-    echo "Error: Missing required argument -airflow_dir <path>"
-    exit 1
+  echo "Error: --airflow_dir parameter is required."
+  exit 1
 fi
 
 mkdir -p $airflow_dir
